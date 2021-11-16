f0c236c7e64a5efb814adae38bb8122699f18529
Taha Tangülü <tangulutaha@gmail.com>
2021-11-16 15:55:58 +0300

    ilk durum
    Veri tabanları projesi ilk durumu

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Java_SQL_Sorgulama
- Veri Tabanları
+# Veri Tabanları Projesi
+# Java ile SQL Sorgulama
+
+Proje Konusu:
+Java ile sitelerden veri çekip (Selenium kullanmayı planlıyoruz). Bu verileri ise database'e kaydedip yine java ile istediğim sorgulamaları yapıp, yapılan sorgulamaları da .txt ya da excel dosyası olarak çıktılarını almak. Şuan için sadece database üzerinden sorgulamarı yapıp çıktı alınıyor. İlerleyen aşamalarda sitelerden veri çekmeyi de ekleyeceğiz.
---
+++ src/javasql/javasql.java
@@ -0,0 +1,54 @@
+package javasql;
+
+		
+import  java.sql.Connection;		
+import  java.sql.Statement;		
+import  java.sql.ResultSet;		
+import  java.sql.DriverManager;		
+import  java.sql.SQLException;
+
+
+
+public class  javasql {				
+    	public static void  main(String[] args) throws  ClassNotFoundException, SQLException {	
+    		
+				// Baglantı için yazış şekili: "jdbc:mysql://ipaddress:portnumber/db_name"		
+                String dbUrl = "jdbc:mysql://localhost:3306/javasql";					
+
+				// Database Kullanıcı adı		
+				String username = "root";	
+                 
+				// Database Parola		
+				String password = "";				
+
+				// Çalıştırılacak sorgu		
+				String query = "select *  from ogrenci;";	
+                
+         	    // mysql jdbc driver		
+           	    Class.forName("com.mysql.jdbc.Driver");			
+           
+           		// Veritabanına bağlantı		
+            	Connection con = DriverManager.getConnection(dbUrl,username,password);
+          
+          		// Nesne oluşturma		
+        	   Statement stmt = con.createStatement();					
+       
+       			// SQL Sorgusunu yürütme ve sonuçları ResultSet'e aktarma	
+         		ResultSet rs= stmt.executeQuery(query);							
+         
+         		// Tüm verileri yinelemek ve sonuçları yazdırmak için Döngü		
+                System.out.println("Ad	"+"Soyad	"+"Yaş	"+"Numara	");
+                System.out.println("-------------------------------");
+				while (rs.next()){
+			        		String Ad = rs.getString(1);
+			        		String Soyad = rs.getString(2);
+                            String Yas = rs.getString(3);
+                            String Num = rs.getString(4);
+                            System.out.println(Ad+"	"+Soyad+"	"+Yas+"	"+Num);
+                            
+                    }		
+      			 // Veritabanı bağlantısını sonlandır.		
+      			con.close();			
+		}
+}
+
